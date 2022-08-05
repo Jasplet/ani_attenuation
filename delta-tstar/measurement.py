79af30cf400625ab74055168088e56ce42b3a2b0
Joseph Asplet <joseph.asplet@bristol.ac.uk>
2022-08-05 16:31:56 +0100

    added function to window traces (to predtermined windows) and finished gridsearch function. now need to build surrounding i/o and find min value

--- delta-tstar/measurement.py
+++ delta-tstar/measurement.py
@@ -9,23 +9,50 @@ Created on Thu Aug  4 16:33:37 2022
 import numpy as np
 import numba
 import scipy.signal
+import obspy
 
-@numba.jit(parallel=True)
-def dtstar_gridsearch(trN, trE):
+from dtstar import apply_tstar_operator
+
+def measure_dtstar(files, snr_max, nfast=181, ndts=81):
     
-    fast_directions = np.arange(-90,91,1)
-    dtstars = np.arange(0,4.05,0.05)
+    #loop over array of sac file names and read
+    difr_stack = np.zeros((nfast, ndts))
+    for file in files:
+        waveform_data = obspy.read(file)
+        waveforms = window_traces(waveform_data)
+        difr_grid = dtstar_gridsearch(waveforms, nfast, ndts)
+        difr_stack += difr_grid
+    # normalise difr_grid by number of files
+    nfiles = len(files)
+    difr_stack = difr_stack/nfiles
+    # find min value
+    grid_shape = (nfast, ndts)
+    idx_min = np.unravel_index(np.argmin(difr_stack),shape=grid_shape)
+
+#@numba.jit(parallel=True)
+def dtstar_gridsearch(waveforms, nfast, ndts, fref=1):
     
-    for i in numba.prange(0,len(fast_directions)):
-        trF, trS = rotate(trN, trE, fast_directions[i])
-        # As we only attenaut the fast trace we can caluclate the inst. freq. for the slow trace now        
-        inst_freq trS = measure_inst_freq(trS)
-        for dtstars in numba.prange(0, len(dtstars)):
+
+    fast_directions = np.linspace(-90,90,nfast)
+    dtstars = np.linspace(0,4.0,ndts)
+
+    difrs = np.zeros((nfast, ndts))
+    for i in numba.prange(0,nfast):
+        fast_slow = waveforms.copy()
+        fast_slow = fast_slow.rotate('NE->RT',fast_directions[i])
+        trF = fast_slow.select(channel='BHR')
+        trS = fast_slow.select(channel='BHT')
+        # As we only attenaute the fast trace we can caluclate the inst. freq.
+        # for the slow trace now        
+        inst_freq_trS = measure_inst_freq(trS)
+        for j in numba.prange(0, ndts):
+            apply_tstar_operator(trF, fref, dtstars[j])
             inst_freq_trF = measure_inst_freq(trF)
-            difr = np.abs(inst_freq_trF - inst_freq_trS)
-            
+            difrs[i,j] = np.abs(inst_freq_trF - inst_freq_trS)
     
-@numba.jit()
+    return difrs
+            
+#@numba.jit()
 def measure_inst_freq(trace):
     '''
     Instantaneous frequency analysis 
@@ -36,8 +63,7 @@ def measure_inst_freq(trace):
     imag_trace = np.imag(hilbert_trace)
     # Calculate instantaneous amplitude, phase, frequency followign scipy docs
     inst_amplitude = np.abs(hilbert_trace)
-    inst_phase = np.atan(imag_trace/real_trace)
-    
+    #inst_phase = np.atan(imag_trace/real_trace)
     delta = trace.stats.delta
     imag_gradient = np.gradient(imag_trace, delta)
     real_gradient = np.gradient(real_trace, delta)
@@ -48,6 +74,15 @@ def measure_inst_freq(trace):
                                       
     return weighted_inst_f
 
-def window_traces(st, wbeg, wend):
+#@numba.jit()
+def window_traces(st):
     
-    pass
+    st_func = st.copy()
+    times = st_func[0].times() + st_func[0].stats.sac.b
+    wbeg = st_func[0].stats.sac.a
+    wend = st_func[0].stats.sac.f
+    window = (times >= wbeg) & (times <= wend)
+    for trace in st_func:
+        trace.data = trace.data[window]
+        
+    return st_func
