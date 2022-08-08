da1f763e69c832aebe0623b6b961ef6ecd5efa01
Joseph Asplet <ja17375@it042614.users.bris.ac.uk>
2022-08-08 17:53:25 +0100

    __init__

---
+++ ani_attenuation/__init__.py
@@ -0,0 +1,8 @@
+name = "delta-tstar" #Â Better name tbd
+__version__ = "0.0.1"
+__description__ = "delta-tstar - A package for measuring attenuation anisotropy (dt*) in shear-waves using instantaneous frequency matching"
+__license__ = "MIT"
+__author__ = "Joseph Asplet, James Wookey"
+__email__ = "joseph.asplet@bristol.ac.uk"
+
+# Package imports go here as/when needed
