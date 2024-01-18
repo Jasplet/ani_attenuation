5d1769b67995cc46ea281bc4b9cac177db248db9
Joseph Asplet <joseph.asplet@earth.ox.ac.uk>
2024-01-18 10:22:51 +0000

    added setup.py

---
+++ setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup, find_packages 
+
+setup(name='ani_attenuation', version='0.1', author='Joseph Asplet', url='http://www.github.com/Jasplet/ani_attenuation', packages=find_packages())
